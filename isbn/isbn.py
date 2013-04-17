da1a43166792e16b06ed1349e1576b0d11693892
vkarappa1 <vkarappa1@gmail.com>
2013-04-16 21:09:48 -0500

    isbn search function

--- isbn/isbn.py
+++ isbn/isbn.py
@@ -1,15 +1,27 @@
 import urllib
+import re
 from bs4 import BeautifulSoup
 
-def getisbnData(keywords):
-	
+
+def getisbnData(query):
+	keywords = re.split(r"[\W]+",query)
+	value = ''
+	i = 0
+	size = len(keywords)
+	while i < size:
+		if i==0:
+			value = keywords[i]
+		else:
+			value = value + '+' +  keywords[i]
+		i = i + 1
+
+	isbnurl = 'https://isbndb.com/api/books.xml?access_key=IMHDRIT9&index1=title&value1=' + value
+
+  	r = urllib.urlopen(isbnurl)
+  	isbn = BeautifulSoup(r)
+  	booksdata = isbn.isbndb.booklist
 
 
 if __name__ == "__main__":
-	r = urllib.urlopen('https://isbndb.com/api/books.xml?access_key=IMHDRIT9&index1=title&value1=thief+of+time')
-	isbn = BeautifulSoup(r)
-
-	
-	booksdata = isbn.isbndb.booklist
-	print(booksdata.contents[3])	
+	 getisbnData('the theif of time')
 	
