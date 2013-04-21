6e529c2a4ac509d9124fe5b8483edc7eb0b271eb
Vardharaj L <vadi1234@gmail.com>
2013-04-21 17:14:49 -0500

    linkedin api

---
+++ linkedin/trial.py
@@ -0,0 +1,17 @@
+from linkedin import linkedin
+
+API_KEY = 'f2wy1ympdfcr'
+API_SECRET = 'wjXlB8Czxplt1mYK'
+RETURN_URL = 'http://psi.cse.tamu.edu/people/virendra-karappa'
+
+authentication = linkedin.LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
+print authentication.authorization_url  # open this url on your browser
+application = linkedin.LinkedInApplication(authentication)
+
+authentication.authorization_code = raw_input("Enter code")
+authentication.get_access_token()
+#application.get_profile()
+print application.get_profile(selectors=['id', 'first-name', 'last-name', 'location', 'distance', 'num-connections', 'skills', 'educations', 'summary', 'interests', 'recommendations-received', 'following', 'courses'])
+
+
+#print application.get_connections()
