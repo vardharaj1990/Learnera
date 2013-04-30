508e75bed6e69a49f55aa8c1db9b6c1a779e4da4
Vardharaj L <vadi1234@gmail.com>
2013-04-30 04:55:07 -0500

    Removing prints

---
+++ Learnera/youtubecatdict.py
@@ -0,0 +1,29 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import cPickle as pickle
+import json
+import urllib2
+import simplejson
+import codecs
+
+courserayoutubemap = defaultdict(str)
+
+def getCourseraMap():
+	file = open('coursera_categories.txt','r')
+	content = file.readlines()
+	count = 1;
+	youtubecat = ""
+	courseracat = ""
+	for line in content:
+		if count == 1:
+			courseracat = line
+			count = count + 1
+			continue
+		elif count == 2:
+			youtubecat = line
+			courserayoutubemap[courseracat.strip('\n')] = youtubecat.strip()
+			count = 1
+	return courserayoutubemap
+			
